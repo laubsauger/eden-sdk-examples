e5981dc66fa0891ad1553003ee0e12d43c8b1c1f
laubsauger <laubsauger@gmail.com>
2023-10-01 15:07:11 +0400

    add generator/create example

--- examples/generators/create.js
+++ examples/generators/create.js
@@ -1,3 +1,5 @@
+import eden from '../../init_eden.js'
+
 const config = {
   text_input: "Garden of Eden"
 }
@@ -8,3 +10,5 @@ const input = {
 }
 
 const result = await eden.tasks.create(input);
+
+console.log(result)
